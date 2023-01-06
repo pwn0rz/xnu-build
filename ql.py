de31b3a7ad8e81764f12fcb6f8afae2b6b75ac22
pwnorz <junzhi0301@gmail.com>
2023-01-06 11:16:37 +0800

    It's strange that CodeQL still can not index all files

--- ql.py
+++ ql.py
@@ -8,13 +8,17 @@ BUILD_DIR = os.environ.get('BUILD_DIR', f'{WORK_DIR}/build')
 FAKEROOT_DIR = os.environ.get('FAKEROOT_DIR', f'{WORK_DIR}/fakeroot')
 
 SRCROOT = f'{WORK_DIR}/xnu'
-OBJROOT = f'{BUILD_DIR}/obj'
-SYMROOT = f'{BUILD_DIR}/sym'
+OBJROOT = f'{BUILD_DIR}/xnu-codeql.obj'
+SYMROOT = f'{BUILD_DIR}/xnu-codeql.sym'
 DSTROOT = f'{FAKEROOT_DIR}'
 
 # TARGET_CONFIGS may stop codeql command expandingg
-BUILD_CMD = f'make install SDKROOT=macosx ARCH_CONFIGS=ARM64 KERNEL_CONFIGS=RELEASE MACHINE_CONFIGS=VMAPPLE LOGCOLORS=y BUILD_WERROR=0 BUILD_LTO=0 SRCROOT={SRCROOT} OBJROOT={OBJROOT} SYMROOT={SYMROOT} DSTROOT={DSTROOT} FAKEROOT={FAKEROOT_DIR} KDKROOT={KDKROOT}'
+BUILD_CMD = f'make install SDKROOT=macosx ARCH_CONFIGS=ARM64 KERNEL_CONFIGS=RELEASE MACHINE_CONFIGS=VMAPPLE BUILD_WERROR=0 BUILD_LTO=0 SRCROOT={SRCROOT} OBJROOT={OBJROOT} SYMROOT={SYMROOT} DSTROOT={DSTROOT} FAKEROOT={FAKEROOT_DIR} KDKROOT={KDKROOT}'
+
+DATABASE_DIR = f'{WORK_DIR}/xnu-codeql'
 
 print(BUILD_CMD)
-os.system(f'rm -r {BUILD_DIR}')
-os.system(f'codeql database create xnu-codeql --language=cpp  --source-root {SRCROOT} --command="{BUILD_CMD}"')
+os.system(f'rm -rf {OBJROOT}')
+os.system(f'rm -rf {SYMROOT}')
+os.chdir(SRCROOT)
+os.system(f'codeql database create {DATABASE_DIR} --overwrite --language=cpp --command="{BUILD_CMD}"')
